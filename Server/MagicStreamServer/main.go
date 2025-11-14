94a15b9b3eb85e4d24e6f8c0053e81bf68a5b5f3
Diego Brito <dibbrito@gmail.com>
2025-11-14 10:30:00 -0300

    feat: load env in main

--- Server/MagicStreamServer/main.go
+++ Server/MagicStreamServer/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -12,6 +14,11 @@ func main() {
 	router.GET("/hello", func(c *gin.Context) {
 		c.String(200, "Hello, Magic Stream!")
 	})
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Println("Warning: unable to find .env file")
+	}
 
 	if err := router.Run(":8080"); err != nil {
 		fmt.Println("Failed to start server", err)

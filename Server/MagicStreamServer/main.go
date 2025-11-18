56773b3447c46f1c6d7f76d8f5f8bb50abdcc960
Diego Brito <dibbrito@gmail.com>
2025-11-18 11:34:40 -0300

    feat: Add route for fetching movies using GetMovies handler

--- Server/MagicStreamServer/main.go
+++ Server/MagicStreamServer/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	controller "github.com/diegobbrito/MagicStream/Server/MagicStreamServer/controllers"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -19,6 +20,8 @@ func main() {
 	if err != nil {
 		log.Println("Warning: unable to find .env file")
 	}
+
+	router.GET("/movies", controller.GetMovies())
 
 	if err := router.Run(":8080"); err != nil {
 		fmt.Println("Failed to start server", err)

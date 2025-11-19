d9a3df9143f3e29b653bd64961bdda2b179fa7f1
Diego Brito <dibbrito@gmail.com>
2025-11-19 17:15:18 -0300

    feat: Implement GetMovies handler to fetch and return a list of movies from the database

--- Server/MagicStreamServer/controllers/movie_controller.go
+++ Server/MagicStreamServer/controllers/movie_controller.go
@@ -1,13 +1,40 @@
 package controllers
 
 import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/diegobbrito/MagicStream/Server/MagicStreamServer/database"
+	"github.com/diegobbrito/MagicStream/Server/MagicStreamServer/models"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 )
+
+var movieCollection *mongo.Collection = database.OpenCollection("movies")
 
 func GetMovies() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "List of movies",
-		})
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var movies []models.Movie
+
+		cursor, err := movieCollection.Find(ctx, bson.M{})
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error fetching movies from database"})
+			return
+		}
+
+		defer cursor.Close(ctx)
+
+		if err = cursor.All(ctx, &movies); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error decoding movies"})
+			return
+		}
+
+		c.JSON(http.StatusOK, movies)
 	}
 }

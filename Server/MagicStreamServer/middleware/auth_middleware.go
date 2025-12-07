a60e67d16d719e4181adf80609d8802d7cd98ef1
Diego Brito <dibbrito@gmail.com>
2025-12-07 09:37:13 -0300

    feat: Implement AuthMiddleware for token validation and user context setting

---
+++ Server/MagicStreamServer/middleware/auth_middleware.go
@@ -0,0 +1,32 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/diegobbrito/MagicStream/Server/MagicStreamServer/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token, err := utils.GetAccessToken(c)
+
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+			c.Abort()
+			return
+		}
+
+		claims, err := utils.ValidateToken(token)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid or expired token"})
+			c.Abort()
+			return
+		}
+
+		c.Set("userId", claims.UserId)
+		c.Set("role", claims.Role)
+
+		c.Next()
+	}
+}

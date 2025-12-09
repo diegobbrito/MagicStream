e670b6796ffd6e51c2bfa1821c1650ac63a1ef3d
Diego Brito <dibbrito@gmail.com>
2025-12-09 19:54:39 -0300

    feat: Add AdminReviewUpdate and GetReviewRanking functions for movie review management

--- Server/MagicStreamServer/controllers/movie_controller.go
+++ Server/MagicStreamServer/controllers/movie_controller.go
@@ -2,14 +2,19 @@ package controllers
 
 import (
 	"context"
+	"errors"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/diegobbrito/MagicStream/Server/MagicStreamServer/database"
 	"github.com/diegobbrito/MagicStream/Server/MagicStreamServer/models"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"github.com/joho/godotenv"
+	"github.com/tmc/langchaingo/llms/openai"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
@@ -73,6 +78,130 @@ func GetMovie() gin.HandlerFunc {
 
 		c.JSON(http.StatusOK, movie)
 	}
+}
+
+func AdminReviewUpdate() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		movieID := c.Param("imdb_id")
+
+		if movieID == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Movie ID is required"})
+			return
+		}
+
+		var req struct {
+			AdminReview string `json:"admin_review"`
+		}
+		var resp struct {
+			RankingName string `json:"ranking_name"`
+			AdminReview string `json:"admin_review"`
+		}
+
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+			return
+		}
+
+		sentiment, rankVal, err := GetReviewRanking(req.AdminReview)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error getting review ranking"})
+			return
+		}
+
+		filter := bson.M{"imdb_id": movieID}
+		update := bson.M{
+			"$set": bson.M{
+				"admin_review": req.AdminReview,
+				"ranking": bson.M{
+					"ranking_name":  sentiment,
+					"ranking_value": rankVal,
+				},
+			},
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		result, err := movieCollection.UpdateOne(ctx, filter, update)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error updating movie review"})
+			return
+		}
+
+		if result.MatchedCount == 0 {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Movie not found"})
+			return
+		}
+
+		resp.RankingName = sentiment
+		resp.AdminReview = req.AdminReview
+
+		c.JSON(http.StatusOK, resp)
+
+	}
+}
+
+func GetReviewRanking(admin_review string) (string, int, error) {
+	rankings, err := GetRankings()
+	if err != nil {
+		return "", 0, err
+	}
+
+	sentimentDelimited := ""
+	for _, ranking := range rankings {
+		if ranking.RankingValue != 999 {
+			sentimentDelimited += ranking.RankingName + ","
+		}
+	}
+
+	sentimentDelimited = strings.Trim(sentimentDelimited, ",")
+
+	err = godotenv.Load(".env")
+
+	if err != nil {
+		log.Println("Warning: .env file not found")
+	}
+
+	OpenAiApiKey := os.Getenv("OPENAI_API_KEY")
+
+	if OpenAiApiKey == "" {
+		return "", 0, errors.New("OpenAI API key not found in environment variables")
+	}
+
+	llm, err := openai.New(openai.WithToken(OpenAiApiKey))
+
+	if err != nil {
+		return "", 0, err
+	}
+
+	base_prompt_template := os.Getenv("BASE_PROMPT_TEMPLATE")
+
+	if base_prompt_template == "" {
+		return "", 0, errors.New("base prompt template not found in environment variables")
+	}
+
+	base_prompt := strings.Replace(base_prompt_template, "{rankings}", sentimentDelimited, 1)
+
+	response, err := llm.Call(context.Background(), base_prompt+admin_review)
+
+	if err != nil {
+		return "", 0, err
+	}
+
+	rankVal := 0
+
+	for _, ranking := range rankings {
+		if ranking.RankingName == response {
+			rankVal = ranking.RankingValue
+			break
+		}
+	}
+
+	return response, rankVal, nil
+
 }
 
 func AddMovie() gin.HandlerFunc {

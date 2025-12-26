7dff6dcb35700352f5cedd30f22e138c5e2ba139
Diego Brito <dibbrito@gmail.com>
2025-12-26 12:15:13 -0300

    refactor: Update SetupProtectedRoutes to accept MongoDB client and pass it to controller functions

--- Server/MagicStreamServer/routes/protected_routes.go
+++ Server/MagicStreamServer/routes/protected_routes.go
@@ -4,14 +4,15 @@ import (
 	controller "github.com/diegobbrito/MagicStream/Server/MagicStreamServer/controllers"
 	"github.com/diegobbrito/MagicStream/Server/MagicStreamServer/middleware"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
-func SetupProtectedRoutes(router *gin.Engine) {
+func SetupProtectedRoutes(router *gin.Engine, client *mongo.Client) {
 	router.Use(middleware.AuthMiddleware())
 
-	router.GET("/movies/:imdb_id", controller.GetMovie())
-	router.POST("/movies", controller.AddMovie())
+	router.GET("/movies/:imdb_id", controller.GetMovie(client))
+	router.POST("/movies", controller.AddMovie(client))
 
-	router.GET("/movies/recommendations", controller.GetRecommendedMovies())
-	router.PATCH("/movies/:imdb_id", controller.AdminReviewUpdate())
+	router.GET("/movies/recommendations", controller.GetRecommendedMovies(client))
+	router.PATCH("/movies/:imdb_id", controller.AdminReviewUpdate(client))
 }

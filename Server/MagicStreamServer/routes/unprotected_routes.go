7b436c28bd3c9a4c4ab6a40a699889d3b2e649db
Diego Brito <dibbrito@gmail.com>
2025-12-27 08:55:07 -0300

    refactor: Update SetupUnprotectedRoutes to accept MongoDB client and pass it to controller functions

--- Server/MagicStreamServer/routes/unprotected_routes.go
+++ Server/MagicStreamServer/routes/unprotected_routes.go
@@ -3,10 +3,11 @@ package routes
 import (
 	controller "github.com/diegobbrito/MagicStream/Server/MagicStreamServer/controllers"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
-func SetupUnprotectedRoutes(router *gin.Engine) {
-	router.GET("/movies", controller.GetMovies())
-	router.POST("/register", controller.RegisterUser())
-	router.POST("/login", controller.LoginUser())
+func SetupUnprotectedRoutes(router *gin.Engine, client *mongo.Client) {
+	router.GET("/movies", controller.GetMovies(client))
+	router.POST("/register", controller.RegisterUser(client))
+	router.POST("/login", controller.LoginUser(client))
 }

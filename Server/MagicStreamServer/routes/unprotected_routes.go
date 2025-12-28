c561307ab540f7cdecf13dd1cd37821b2e9e816d
Diego Brito <dibbrito@gmail.com>
2025-12-28 13:28:09 -0300

    feat: Add Logout route to unprotected routes for user logout functionality

--- Server/MagicStreamServer/routes/unprotected_routes.go
+++ Server/MagicStreamServer/routes/unprotected_routes.go
@@ -10,4 +10,5 @@ func SetupUnprotectedRoutes(router *gin.Engine, client *mongo.Client) {
 	router.GET("/movies", controller.GetMovies(client))
 	router.POST("/register", controller.RegisterUser(client))
 	router.POST("/login", controller.LoginUser(client))
+	router.POST("/logout", controller.LogoutHandler(client))
 }

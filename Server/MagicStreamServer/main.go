9e42bc1843775a268bfd751340c2a7b5c8d47e29
Diego Brito <dibbrito@gmail.com>
2026-01-11 08:49:05 -0300

    feat: add CORS configuration to main server setup

--- Server/MagicStreamServer/main.go
+++ Server/MagicStreamServer/main.go
@@ -5,9 +5,11 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/diegobbrito/MagicStream/Server/MagicStreamServer/database"
 	"github.com/diegobbrito/MagicStream/Server/MagicStreamServer/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/v2/mongo"
@@ -34,6 +36,16 @@ func main() {
 		origins = []string{"http://localhost:5173"}
 		log.Println("Allowed Origin: http://localhost:5173")
 	}
+
+	config := cors.Config{}
+	config.AllowOrigins = origins
+	config.AllowMethods = []string{"GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"}
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization"}
+	config.ExposeHeaders = []string{"Content-Length"}
+	config.AllowCredentials = true
+	config.MaxAge = 12 * time.Hour
+
+	router.Use(cors.New(config))
 
 	PORT := os.Getenv("PORT")
 	if PORT == "" {

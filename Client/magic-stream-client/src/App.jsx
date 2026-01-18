ba7734352d85c67ce0383989f4924ec9671a0d3a
Diego Brito <dibbrito@gmail.com>
2026-01-18 08:35:47 -0300

    feat: add Header component to App for improved layout

--- Client/magic-stream-client/src/App.jsx
+++ Client/magic-stream-client/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './components/home/Home.jsx'
+import Header from './components/header/Header.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
       <Home />
     </>
   )

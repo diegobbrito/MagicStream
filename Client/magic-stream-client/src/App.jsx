83a154d6b579a71e6701788d46c8e5e2259c6105
Diego Brito <dibbrito@gmail.com>
2026-01-20 20:59:40 -0300

    feat: implement routing for Home, Register, and Login components

--- Client/magic-stream-client/src/App.jsx
+++ Client/magic-stream-client/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import './App.css'
 import Home from './components/home/Home.jsx'
 import Header from './components/header/Header.jsx'
+import { Route, Routes } from 'react-router-dom'
+import Register from './components/register/Register.jsx'
+import Login from './components/login/Login.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +12,11 @@ function App() {
   return (
     <>
       <Header />
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
     </>
   )
 }

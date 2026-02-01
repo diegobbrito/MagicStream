0a78272cd82584bf215af9b2b2f70fdb169b7b7d
Diego Brito <dibbrito@gmail.com>
2026-02-01 07:40:32 -0300

    feat: update Routes component to use Layout for nested routing

--- Client/magic-stream-client/src/App.jsx
+++ Client/magic-stream-client/src/App.jsx
@@ -4,13 +4,14 @@ import Header from './components/header/Header.jsx'
 import { Route, Routes } from 'react-router-dom'
 import Register from './components/register/Register.jsx'
 import Login from './components/login/Login.jsx'
+import Layout from './components/Layout.jsx'
 
 function App() {
 
   return (
     <>
       <Header />
-      <Routes>
+      <Routes path="/" element = {Layout}>
         <Route path='/' element={<Home />} />
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />

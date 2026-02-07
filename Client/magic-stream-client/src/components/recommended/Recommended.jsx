9804493744248dc683900a5b2056b4f5808f2a82
Diego Brito <dibbrito@gmail.com>
2026-02-07 11:39:33 -0300

    feat: add Recommended component for fetching and displaying movie recommendations

---
+++ Client/magic-stream-client/src/components/recommended/Recommended.jsx
@@ -0,0 +1,40 @@
+import useAxiosPrivate from "../../hook/useAxiosPrivate";
+import { useEffect, useState } from "react";
+import Movies from "../movies/Movies";
+
+const Recommended = () => {
+   const [movies, setMovies] = useState([]);
+   const [loading, setLoading] = useState(false);
+   const [message, setMessage] = useState();
+   const axiosPrivate = useAxiosPrivate();
+
+   useEffect(() => {
+    const fetchRecommendedMovies = async () => {
+        setLoading(true);
+        setMessage("");
+
+        try {
+            const response = await axiosPrivate.get('/movies/recommendations');
+            setMovies(response.data);
+        } catch (err) {
+            console.error('Error fetching recommended movies:', err);
+            setMessage("Failed to load recommendations. Please try again later.");
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    fetchRecommendedMovies();
+   }, [])
+
+    return (
+        <>
+            {loading ? (
+                <h2>Loading recommendations...</h2>
+            ) : <Movies movies={movies} message={message} />}
+        </>
+    )
+}
+
+export default Recommended;
+

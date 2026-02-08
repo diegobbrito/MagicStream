dba9987b2ba9ad663191d0c4ea7eb3d74afa4608
Diego Brito <dibbrito@gmail.com>
2026-02-08 11:08:34 -0300

    feat: enable Recommended route for displaying movie recommendations

--- Client/magic-stream-client/src/App.jsx
+++ Client/magic-stream-client/src/App.jsx
@@ -6,6 +6,7 @@ import Register from './components/register/Register.jsx'
 import Login from './components/login/Login.jsx'
 import Layout from './components/Layout.jsx'
 import RequiredAuth from './components/RequiredAuth.jsx'
+import Recommended from './components/recommended/Recommended.jsx'
 
 function App() {
 
@@ -17,7 +18,7 @@ function App() {
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
         <Route element = {<RequiredAuth/>}>
-          {/* <Route path='/recommended' element={<Recommended />} /> */}
+          <Route path='/recommended' element={<Recommended />} />
         </Route>
       </Routes>
     </>

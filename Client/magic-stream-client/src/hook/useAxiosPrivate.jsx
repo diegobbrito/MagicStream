f27dae16c0be7050fb295f90595ad4378e88b1d3
Diego Brito <dibbrito@gmail.com>
2026-02-20 18:02:29 -0300

    feat: enable withCredentials in axios configuration for secure requests

--- Client/magic-stream-client/src/hook/useAxiosPrivate.jsx
+++ Client/magic-stream-client/src/hook/useAxiosPrivate.jsx
@@ -1,5 +1,4 @@
 import axios from "axios";
-import useAuth from "./useAuth";
 
 const apiUrl = import.meta.env.VITE_API_BASE_URL;
 
@@ -7,15 +6,6 @@ const useAxiosPrivate = () => {
     const axiosAuth = axios.create({
         baseURL: apiUrl,
         withCredentials: true,
-    })
-
-    const {auth, setAuth} = useAuth();
-
-    axiosAuth.interceptors.request.use((config) => {
-        if (auth){
-            config.headers.Authorization = `Bearer ${auth.token}`;
-        }
-        return config;
     })
 
     return axiosAuth;

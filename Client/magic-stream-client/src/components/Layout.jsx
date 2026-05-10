8835262433de5ab4bc05472a2835c684198b6e03
Diego Brito <dibbrito@gmail.com>
2026-05-10 08:24:05 -0300

    refactor: remove min-height from main styling in Layout and index.css

--- Client/magic-stream-client/src/components/Layout.jsx
+++ Client/magic-stream-client/src/components/Layout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
     return (
-        <main style={{ width: '100%', minHeight: '100vh', padding: '20px 0' }}>
+        <main style={{ width: '100%', padding: '20px 0' }}>
             <div className='container'>
                 <Outlet/>
             </div>
